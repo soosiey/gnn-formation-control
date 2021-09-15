d464ac3806d63c26b4dede9e5ee0cf4f9bb00eda
soosiey <yerapathisuhaas@gmail.com>
2021-09-15 02:33:53 -0400

    plotting for stats

--- position_plot.py
+++ position_plot.py
@@ -1,13 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_average(arr):
-    mean = np.mean(arr,axis=1)
-    diff = np.subtract(arr,mean.reshape((arr.shape[0],1,arr.shape[2])))
-    norm = np.linalg.norm(diff,axis=2)
-    avgs = np.mean(norm,axis=1)
-    average = np.mean(avgs)
-    return average
+def get_average(arr,n):
+    mean_total = 0
+    st_total = 0
+    for i in range(arr.shape[0]):
+        current = arr[i,-n:,:]
+        m = np.mean(current,axis=0)
+        d = np.subtract(current,m.reshape((1,2)))
+        d = d * d
+        s = np.sum(d,axis=1)
+        s = np.sqrt(s)
+        m = np.mean(s)
+        st = np.std(s)
+        mean_total += m
+        st_total += (st*st)
+    mean_total = mean_total / arr.shape[0]
+    st_total = st_total / arr.shape[0]
+    st_total = np.sqrt(st_total)
+    return mean_total,st_total
 
 def get_running_average(arr,n):
 
@@ -29,29 +40,114 @@ def get_running_average(arr,n):
         mean_total = mean_total / arr.shape[0]
         y.append(mean_total)
     return y
-def get_end():
-    ynet = []
-    yexpert = []
-    for i in range(8,9):
 
-        network = np.load('positionList_network_'+str(i)+'.npy')
-        expert = np.load('positionList_expert_'+str(i)+'.npy')
-        ynet.append(get_average(network))
-        yexpert.append(get_average(expert))
+def checkmove(arr):
+    moving = False
+    for i in range(1,len(arr)):
+        if(abs(arr[i] - arr[i-1]) > .001):
+            moving = True
+    return moving
+
+def get_convergence(arr,n):
+    window_size = n*10
+    ct_avg = 0
+    ct_count = 0
+    for i in range(10):
+        current = arr[i]
+        convergence = False
+        tf = None
+        for j in range(n*100,current.shape[0] - window_size,n):
+            if(convergence):
+                break
+            window = current[j:j+window_size]
+            count = 0
+            for k in range(n):
+                robot_window = window[k::n]
+                check = []
+                for t in range(robot_window.shape[0]):
+                    d = (robot_window[t,0]**2 + robot_window[t,1]**2)**(.5)
+                    check.append(d)
+                m = checkmove(check)
+                if(not m):
+                    count += 1
+            if(count >= n):
+                convergence = True
+                tf = j//n
+        if(type(tf) == type(None)):
+            continue
+        else:
+            ct_avg += tf
+            ct_count += 1
+    if(ct_count != 0):
+        return ct_avg // ct_count
+    else:
+        return -1
 def main():
 
     colors = ['blue','orange','green','red','purple','brown']
     x = [[] for i in range(6)]
     xn = [[] for i in range(6)]
+
+
     for i in range(3,9):
+        expert = np.load('positionList_network_'+str(i)+'.npy')
+        network = np.load('positionList_expert_'+str(i)+'.npy')
         xn[i - 3] = get_running_average(np.load('positionList_network_'+str(i)+'.npy'),i)
         x[i - 3] = get_running_average(np.load('positionList_expert_'+str(i)+'.npy'),i)
 
-    plt.plot()
+    plt.figure()
     for i in range(3,9):
         plt.plot(xn[i - 3],label=str(i),color=colors[i-3],ls='dotted')
         plt.plot(x[i - 3],label=str(i),color=colors[i-3])
+    plt.xlabel('Timestep (tick = .05 s)')
+    plt.ylabel('Average distance from center of mass of formation')
+    plt.title('Average distance from center of formation during episode')
     plt.legend()
+    plt.savefig('time_graph.png')
     plt.show()
+
+    network_means = []
+    expert_means = []
+    network_std = []
+    expert_std = []
+    ticks = np.arange(3,9)
+    plt.figure()
+    for i in range(3,9):
+        expert = np.load('positionList_network_'+str(i)+'.npy')
+        network = np.load('positionList_expert_'+str(i)+'.npy')
+        em,es = get_average(expert,i)
+        nm,ns = get_average(network,i)
+        network_means.append(xn[i-3][-1])
+        expert_means.append(x[i-3][-1])
+        network_std.append(ns)
+        expert_std.append(es)
+    plt.bar(ticks,expert_means,.4,yerr=expert_std,label='expert')
+    plt.bar(ticks+.5,network_means,.4,yerr=network_std,label='network')
+    plt.xticks(ticks+.5/2,ticks)
+    plt.xlabel('Number of robots')
+    plt.ylabel('Distance from center of formation')
+    plt.title('Ending distance from center of formation')
+    plt.legend()
+    plt.savefig('distance_graph.png')
+    plt.show()
+
+    for i in range(3,9):
+        plt.figure()
+        expert = np.load('positionList_network_'+str(i)+'.npy')
+        network = np.load('positionList_expert_'+str(i)+'.npy')
+        cn = get_convergence(network,i)
+        ce = get_convergence(expert,i)
+        plt.plot(xn[i - 3],color=colors[i-3],ls='dotted',label='network')
+        plt.plot(x[i - 3],color=colors[i-3],label='expert')
+        if(cn != -1):
+            plt.vlines(cn,0,3,ls='dotted',color=colors[i-3])
+        if(ce != -1):
+            plt.vlines(ce,0,3,color=colors[i-3])
+        plt.xlabel('Time (tick = .05 s)')
+        plt.ylabel('Distance from center of formation')
+        plt.title('Distance from center of formation and convergence time for ' + str(i) + ' robots')
+        plt.legend()
+        plt.savefig('convergence_'+str(i)+'.png')
+        plt.show()
 main()
 
