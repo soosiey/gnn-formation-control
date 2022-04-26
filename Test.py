43a5c1bf38d68f49c42c49f041a1771259d25065
xinchi <xh1255@nyu.edu>
2022-04-26 18:23:50 -0400

    add new stoping condition add test

--- Test.py
+++ Test.py
@@ -32,8 +32,8 @@ parser.add_argument('--if_train', dest='if_train', default=False,type=bool,help=
 parser.add_argument('--if_continue', dest='if_continue', default=False,type=bool,help='Continue training')
 parser.add_argument('--expert_velocity_adjust', dest='expert_velocity_adjust', default=True,type=bool,help=' Adjust controller output accoring to the ralative distance output when using expert control')
 parser.add_argument('--model_path', dest='model_path', default='models',type=str,help='Path to save model')
-parser.add_argument('--model_name', dest='model_name', default='model_train_episode-160_robot-5.pth',type=str,help='Name of model')
-parser.add_argument('--robot_num', dest='robot_num', default=4,type=int,help='Number of robot for simulation')
+parser.add_argument('--model_name', dest='model_name', default='model_train_episode-150_robot-5.pth',type=str,help='Name of model')
+parser.add_argument('--robot_num', dest='robot_num', default=5,type=int,help='Number of robot for simulation')
 parser.add_argument('--position_range', dest='position_range', default=5,type=int,help='Set robots position within the range')
 parser.add_argument('--sim_dt', dest='sim_dt', default=0.05,type=float,help='Simulation time step')
 parser.add_argument('--sim_time', dest='sim_time', default=200,type=float,help='Simulation time for one simulation')
@@ -67,7 +67,7 @@ def Test(args):
             position_list.append(position)
         print(position_list)
         #### Test model result
-        model_type="expert_adjusted_4"
+        model_type="expert_adjusted_5"
         print(model_type)
         sc=set_robot_positions(sc, position_list)
         sc0 = simulate(args.sim_time,args.sim_dt,args.stop_waiting_time,args.desire_distance,args.stop_thresh,sc)
@@ -77,7 +77,7 @@ def Test(args):
         sc = generate_scene(args.sim_dt, 0, args.robot_num, args.if_train, args.expert_only, args.use_dagger, args.sim_time,
                             args.position_range,
                             args.desire_distance, args.stop_thresh, args.expert_velocity_adjust, agent=fcl)
-        model_type = "model_4"
+        model_type = "model_5"
         print(model_type)
         print(position_list)
         sc=set_robot_positions(sc, position_list)
